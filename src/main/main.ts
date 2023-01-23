9a7454e98beddb4139f6384112596b0750583057
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-01-23 10:06:31 +0900

    設定ファイルに移行（暫定でtsファイル）

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,94 +1,8 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import { XMLParser } from "fast-xml-parser";
 import path from "path";
 import { promises as fs } from "fs";
-
-const listOfIsland: { [name: string]: { offsetX: number; offsetY: number } } = {
-  "mega_island_15_2.xml": {
-    offsetX: 7000,
-    offsetY: -10000
-  },
-  "mega_island_15_3.xml": {
-    offsetX: 7000,
-    offsetY: -9000
-  },
-  "mega_island_15_4.xml": {
-    offsetX: 7000,
-    offsetY: -8000
-  },
-  "mega_island_14_2.xml": {
-    offsetX: 6000,
-    offsetY: -10000
-  },
-  "mega_island_14_3.xml": {
-    offsetX: 6000,
-    offsetY: -9000
-  },
-  "mega_island_14_4.xml": {
-    offsetX: 6000,
-    offsetY: -8000
-  },
-  "mega_island_14_5.xml": {
-    offsetX: 6000,
-    offsetY: -7000
-  },
-  "mega_island_13_3.xml": {
-    offsetX: 5000,
-    offsetY: -9000
-  },
-  "mega_island_13_4.xml": {
-    offsetX: 5000,
-    offsetY: -8000
-  },
-  "mega_island_13_5.xml": {
-    offsetX: 5000,
-    offsetY: -7000
-  },
-  "mega_island_13_6.xml": {
-    offsetX: 5000,
-    offsetY: -6000
-  },
-  "mega_island_12_4.xml": {
-    offsetX: 4000,
-    offsetY: -8000
-  },
-  "mega_island_12_6.xml": {
-    offsetX: 4000,
-    offsetY: -6000
-  },
-  "mega_island_12_7.xml": {
-    offsetX: 4000,
-    offsetY: -5000
-  },
-  "mega_island_12_8.xml": {
-    offsetX: 4000,
-    offsetY: -4000
-  },
-  "mega_island_11_8.xml": {
-    offsetX: 3000,
-    offsetY: -4000
-  },
-  "mega_island_10_8.xml": {
-    offsetX: 2000,
-    offsetY: -4000
-  },
-  "mega_island_9_8.xml": {
-    offsetX: 1000,
-    offsetY: -4000
-  },
-  "mega_island_9_7.xml": {
-    offsetX: 1000,
-    offsetY: -5000
-  },
-  "mega_island_8_8.xml": {
-    offsetX: 0,
-    offsetY: -4000
-  },
-  "mega_island_8_7.xml": {
-    offsetX: 0,
-    offsetY: -5000
-  }
-};
+import { app, BrowserWindow, ipcMain } from "electron";
+import { XMLParser } from "fast-xml-parser";
+import { tiles } from "./tiles";
 
 async function readAndParseXML(_path: string) {
   try {
@@ -99,16 +13,17 @@ async function readAndParseXML(_path: string) {
     });
     return xmlParser.parse(data.toString());
   } catch (e) {
-    throw e;
+    console.error(e);
+    return undefined;
   }
 }
 
 async function handleLoadRomTrack() {
   let list: any = {};
   try {
-    for (const name in listOfIsland) {
-      if (Object.prototype.hasOwnProperty.call(listOfIsland, name)) {
-        const tile = listOfIsland[name];
+    for (const name in tiles) {
+      if (Object.prototype.hasOwnProperty.call(tiles, name)) {
+        const tile = tiles[name];
 
         const xmlContent = await readAndParseXML(
           path.join(__dirname, "../", ".temp", name)
@@ -149,7 +64,7 @@ async function handleLoadAddon() {
     let list: any[] = [];
     if (xmlContent) {
       for (const l of xmlContent.playlist.locations.locations.l) {
-        const tile = listOfIsland[l["@_tile"].replace("data/tiles/", "")];
+        const tile = tiles[l["@_tile"].replace("data/tiles/", "")];
         if (tile) {
           for (const c of l.components.c) {
             list.push({
