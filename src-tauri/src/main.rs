609a3c942bb97513bc30aa1361dfce6857e21c22
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-07-16 11:07:31 +0900

    ファイル読み込み関数をとりあえず定義

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,20 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_file_command(path: String) -> Result<String, String>{
+  let filepath = std::path::Path::new(&path);
+  let content = match std::fs::read_to_string(filepath){
+    Ok(content) => content,
+    Err(e) => return Err(e.to_string()),
+  };
+  Ok(content)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            read_file_command
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
