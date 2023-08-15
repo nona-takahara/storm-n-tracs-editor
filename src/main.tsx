c7e9429829dbbe0a5c4a9d0a8267aee2541e7cce
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-15 18:56:45 +0900

    ImmerのMap, Setサポートを有効化

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { enableMapSet } from "immer";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
+
+enableMapSet();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
