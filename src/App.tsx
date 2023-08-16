2bb7611f8fdc2e9e00aab1af96b091727645dac8
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-16 20:49:41 +0900

    テスト用データ類の消去

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useLayoutEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import Nav from "./ui/Nav";
-import Project from "./data/Project";
 import Vector2d from "./data/Vector2d";
 import StormTracks from "./data/StormTracks";
 import { XMLParser } from "fast-xml-parser";
-import DEBUG_VALUES from "./debug_value.json";
-import DebugView from "./ui/DebugView";
 import { Button } from "@blueprintjs/core";
 import InfoView from "./ui/InfoView";
 import EditStage from "./ui/PixiView/EditStage";
@@ -35,10 +32,8 @@ function read_file_command(filepath: string) {
 
 function App() {
   const [width, height] = useWindowSize();
-
   const [vertexes, updateVertexes] = useImmer(new Map<string, Vector2d>());
   const [areas, updateAreas] = useImmer(new Map<string, AreaPolygon>());
-
   const [tracks, setTracks] = useState<StormTracks[]>([]);
   const [nearestVertex, setNearestVertex] = useState<string | undefined>(
     undefined
@@ -46,46 +41,13 @@ function App() {
   const [selectedArea, setSelectedArea] = useState<string | undefined>(
     undefined
   );
-
   const [editMode, setEditMode] = useState<EditMode.EditMode>(
     EditMode.EditArea
   );
 
-  function reload() {
-    console.clear();
-    const test = Project.createTestData();
-    updateVertexes(test.vertexes);
-    updateAreas(test.areas);
-    setTracks([]);
-    for (let x = 0; x <= 16; x++) {
-      for (let y = 0; y <= 9; y++) {
-        read_file_command(DEBUG_VALUES.tile_dir + `mega_island_${x}_${y}.xml`)
-          .then((str) => {
-            const xmlParser = new XMLParser({
-              ignoreAttributes: false,
-              ignoreDeclaration: true,
-            });
-            setTracks((old) => {
-              const list = old.slice();
-              list[x * 10 + y] = StormTracks.loadFromXML(
-                (x - 8) * 1000,
-                y * 1000 - 12000,
-                xmlParser.parse(str)
-              );
-              return list;
-            });
-          })
-          .catch(() => console.log(x, y));
-      }
-    }
-  }
-
   return (
     <>
-      <Nav></Nav>
-      <DebugView>
-        <Button onClick={reload}>Reload</Button>
-      </DebugView>
+      <Nav />
       <InfoView
         vertexes={vertexes}
         updateVertexes={updateVertexes}
