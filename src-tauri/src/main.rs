d3069f24b693e037eb06739823c9fa3d1be3aca8
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-16 20:37:19 +0900

    ファイルダイアログ回りをセットアップ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,24 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std;
+use std::{self, path::PathBuf};
+use tauri::api::dialog::blocking::FileDialogBuilder;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn read_file_command(path: String) -> Result<String, String>{
-  let filepath = std::path::Path::new(&path);
-  let content = match std::fs::read_to_string(filepath){
-    Ok(content) => content,
-    Err(e) => return Err(e.to_string()),
-  };
-  Ok(content)
+fn read_file_command(path: String) -> Result<String, String> {
+    let filepath = std::path::Path::new(&path);
+    let content = match std::fs::read_to_string(filepath) {
+        Ok(content) => content,
+        Err(e) => return Err(e.to_string()),
+    };
+    Ok(content)
+}
+
+#[tauri::command]
+fn open_file_command() -> Result<String, String> {
+    let path: Option<PathBuf> = FileDialogBuilder::new().pick_file();
+    match path {
+        Some(filepath) => {
+            let content = match std::fs::read_to_string(filepath) {
+                Ok(content) => content,
+                Err(e) => return Err(e.to_string()),
+            };
+            return Ok(content);
+        }
+        None => return Err("File select canceld.".to_string()),
+    };
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            read_file_command
-        ])
+        .invoke_handler(tauri::generate_handler![read_file_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
